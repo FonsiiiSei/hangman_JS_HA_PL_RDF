84c4a8986ca3814b8339928605be13d6cd984de2
Jonas Seidl <seidljonas9gmail.com>
2025-12-10 09:21:28 +0100

    adding function to read randomWord

---
+++ booklist_management.py
@@ -0,0 +1,15 @@
+import random;
+
+def getRandomWord():
+
+
+    return_list = [];
+    with open("random_words.txt", "r", encoding="utf-8") as file:
+        content = file.readlines();
+        for element in content:
+            return_list.append(element.strip())
+
+    return(return_list[random.randint(1, len(content))])
+
+
+print(getRandomWord());       
