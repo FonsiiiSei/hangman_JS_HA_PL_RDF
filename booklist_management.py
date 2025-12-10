c42329f7290bedd5a77087ce3ee0d39d06f7a162
Jonas Seidl <seidljonas9gmail.com>
2025-12-10 09:28:27 +0100

    formatting

--- booklist_management.py
+++ booklist_management.py
@@ -2,14 +2,12 @@ import random;
 
 def getRandomWord():
 
-
     return_list = [];
     with open("random_words.txt", "r", encoding="utf-8") as file:
         content = file.readlines();
         for element in content:
             return_list.append(element.strip())
 
-    return(return_list[random.randint(1, len(content))])
-
+    return(return_list[random.randint(1, len(content))]);
 
 print(getRandomWord());       
